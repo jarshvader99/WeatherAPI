05c9a60653893d8c37436c83d5cce410371807a9
jarshvader99 <jarshvader@gmail.com>
2021-01-19 16:15:52 -0600

    Add project files.

---
+++ WeatherAPI/Controllers/CurrentWeatherController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using WeatherAPI.Models;
+using static WeatherAPI.Models.CurrentWeatherModel;
+
+namespace WeatherAPI.Controllers
+{
+    public class CurrentWeatherController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            CurrentWeatherModel WeatherInfo = new CurrentWeatherModel();
+            string Baseurl = "https://localhost:44319/";
+            using (var client = new HttpClient())
+            {
+                //Passing service base url  
+                client.BaseAddress = new Uri(Baseurl);
+
+                client.DefaultRequestHeaders.Clear();
+                //Define request data format  
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Add("x-rapidapi-key", "1cea7965f6mshe85b862d442ea49p135f78jsne09da033f7b4");
+                client.DefaultRequestHeaders.Add("x-rapidapi-host", "weatherbit-v1-mashape.p.rapidapi.com");
+
+                //Sending request to find web api REST service resource GetAllEmployees using HttpClient  
+                HttpResponseMessage Res = await client.GetAsync("https://weatherbit-v1-mashape.p.rapidapi.com/current?city=Independence,MO&country=US&units=i");
+
+                //Checking the response is successful or not which is sent using HttpClient  
+                if (Res.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api   
+                    var WeatherResponse = Res.Content.ReadAsStringAsync().Result;
+                
+                    //Deserializing the response recieved from web api and storing into the weather Model
+               
+                    WeatherInfo = JsonConvert.DeserializeObject<CurrentWeatherModel>(WeatherResponse);
+
+                }
+                //returning the employee list to view  
+                return View(WeatherInfo);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(string CityName)
+        {
+            CurrentWeatherModel WeatherInfo = new CurrentWeatherModel();
+            string Baseurl = "https://localhost:44319/";
+            using (var client = new HttpClient())
+            {
+                //Passing service base url  
+                client.BaseAddress = new Uri(Baseurl);
+
+                client.DefaultRequestHeaders.Clear();
+                //Define request data format  
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Add("x-rapidapi-key", "1cea7965f6mshe85b862d442ea49p135f78jsne09da033f7b4");
+                client.DefaultRequestHeaders.Add("x-rapidapi-host", "weatherbit-v1-mashape.p.rapidapi.com");
+
+                //Sending request to find web api REST service resource GetAllEmployees using HttpClient  
+                HttpResponseMessage Res = await client.GetAsync("https://weatherbit-v1-mashape.p.rapidapi.com/current?city=" + CityName + "&country=US&units=i");
+
+                //Checking the response is successful or not which is sent using HttpClient  
+                if (Res.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api   
+                    var WeatherResponse = Res.Content.ReadAsStringAsync().Result;
+
+                    //Deserializing the response recieved from web api and storing into the weather Model
+
+                    WeatherInfo = JsonConvert.DeserializeObject<CurrentWeatherModel>(WeatherResponse);
+
+                }
+            }
+                return View(WeatherInfo);
+        }
+    }
+}
---
+++ WeatherAPI/Models/CurrentWeatherModel.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace WeatherAPI.Models
+{
+ 
+        // Root myDeserializedClass = JsonSerializer.Deserialize<Root>(myJsonResponse);
+        public class Weather
+        {
+            [JsonPropertyName("icon")]
+            public string Icon;
+
+            [JsonPropertyName("code")]
+            public int Code;
+
+            [JsonPropertyName("description")]
+            public string Description;
+        }
+
+        public class Datum
+        {
+            [JsonPropertyName("rh")]
+            public double Rh;
+
+            [JsonPropertyName("pod")]
+            public string Pod;
+
+            [JsonPropertyName("lon")]
+            public double Lon;
+
+            [JsonPropertyName("pres")]
+            public double Pres;
+
+            [JsonPropertyName("timezone")]
+            public string Timezone;
+
+            [JsonPropertyName("ob_time")]
+            public string ObTime;
+
+            [JsonPropertyName("country_code")]
+            public string CountryCode;
+
+            [JsonPropertyName("clouds")]
+            public double Clouds;
+
+            [JsonPropertyName("ts")]
+            public double Ts;
+
+            [JsonPropertyName("solar_rad")]
+            public double SolarRad;
+
+            [JsonPropertyName("state_code")]
+            public string StateCode;
+
+            [JsonPropertyName("city_name")]
+            public string CityName;
+
+            [JsonPropertyName("wind_spd")]
+            public double WindSpd;
+
+            [JsonPropertyName("wind_cdir_full")]
+            public string WindCdirFull;
+
+            [JsonPropertyName("wind_cdir")]
+            public string WindCdir;
+
+            [JsonPropertyName("slp")]
+            public double Slp;
+
+            [JsonPropertyName("vis")]
+            public double Vis;
+
+            [JsonPropertyName("h_angle")]
+            public double HAngle;
+
+            [JsonPropertyName("sunset")]
+            public string Sunset;
+
+            [JsonPropertyName("dni")]
+            public double Dni;
+
+            [JsonPropertyName("dewpt")]
+            public double Dewpt;
+
+            [JsonPropertyName("snow")]
+            public double Snow;
+
+            [JsonPropertyName("uv")]
+            public double Uv;
+
+            [JsonPropertyName("precip")]
+            public double Precip;
+
+            [JsonPropertyName("wind_dir")]
+            public double WindDir;
+
+            [JsonPropertyName("sunrise")]
+            public string Sunrise;
+
+            [JsonPropertyName("ghi")]
+            public double Ghi;
+
+            [JsonPropertyName("dhi")]
+            public double Dhi;
+
+            [JsonPropertyName("aqi")]
+            public object Aqi;
+
+            [JsonPropertyName("lat")]
+            public double Lat;
+
+            [JsonPropertyName("weather")]
+            public Weather Weather;
+
+            [JsonPropertyName("datetime")]
+            public string Datetime;
+
+            [JsonPropertyName("temp")]
+            public double Temp;
+
+            [JsonPropertyName("station")]
+            public string Station;
+
+            [JsonPropertyName("elev_angle")]
+            public double ElevAngle;
+
+            [JsonPropertyName("app_temp")]
+            public double AppTemp;
+        }
+
+        public class CurrentWeatherModel
+        {
+            [JsonPropertyName("data")]
+            public List<Datum> Data;
+
+            [JsonPropertyName("count")]
+            public int Count;
+        }
+
+    
+}
