f598624cdaaba67ab5773f60fbf358a80fa63f08
jarshvader99 <jarshvader@gmail.com>
2021-01-19 22:58:32 -0600

    adding new api call to bring in 3 day forecast by visiting /Forecast

---
+++ WeatherAPI/Controllers/ForecastController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using WeatherAPI.Models;
+
+namespace WeatherAPI.Controllers
+{
+    public class ForecastController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(string City)
+        {
+            //CurrentWeatherModel WeatherInfo = new CurrentWeatherModel();
+            Root forecastInfo = new Root();
+            string Baseurl = "https://localhost:44319/";
+            var WeatherResponse = "";
+            using (var client = new HttpClient())
+            {
+                //Passing service base url  
+                client.BaseAddress = new Uri(Baseurl);
+
+                client.DefaultRequestHeaders.Clear();
+                //Define request data format  
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Add("x-rapidapi-key", "1cea7965f6mshe85b862d442ea49p135f78jsne09da033f7b4");
+                client.DefaultRequestHeaders.Add("x-rapidapi-host", "weatherapi-com.p.rapidapi.com");
+
+                //Sending request to find web api REST service resource GetAllEmployees using HttpClient  
+                HttpResponseMessage Res = await client.GetAsync("https://weatherapi-com.p.rapidapi.com/forecast.json?q=" + City + "&days=3");
+
+                //Checking the response is successful or not which is sent using HttpClient  
+                if (Res.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api   
+                    WeatherResponse = Res.Content.ReadAsStringAsync().Result;
+
+                    //Deserializing the response recieved from web api and storing into the weather Model
+
+                    forecastInfo = JsonConvert.DeserializeObject<Root>(WeatherResponse);
+
+                }
+            }
+            return View(forecastInfo);
+        }
+    }
+}
